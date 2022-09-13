2cd177e2958c2e1c58831953663baa85ebc82c2f
Fırat Selim ZİREKOĞLU <zirekoglufs@gmail.com>
2022-09-13 23:42:14 +0300

    added color

--- ts/Models/Candle.ts
+++ ts/Models/Candle.ts
@@ -1,42 +1,44 @@
 import * as PIXI from "pixi.js";
-import { CandleBodyDetails, CandleColor, CandleDetails, Coords, NeedleDetails } from "../interfaces";
+import { CandleBodyDetails, CandleColor, CandleDetails, Coord, NeedleDetails } from "../interfaces";
 
 export default class Candle {
     constructor(){
     }
-    create(CandleDetails:CandleDetails,x:number){
+    create(CandleDetails:CandleDetails,Coord:Coord){
         const color = CandleDetails.open - CandleDetails.close > 0 ? CandleColor.Red : CandleColor.Green;
+        const h:number = color == CandleColor.Green ? CandleDetails.close - CandleDetails.high : CandleDetails.close - CandleDetails.low;
+        console.log(h);
         const CandleCon = new PIXI.Container();
-        CandleCon.x = x;
-        CandleCon.y = 100;
-        const h:number = color == CandleColor.Green ? CandleDetails.high - CandleDetails.close : CandleDetails.low - CandleDetails.open;
-        const candleBody = this.createCandleBody(CandleDetails, h, color);
-        const needle = this.createNeedle(CandleDetails, color);
+        CandleCon.x = Coord.x;
+        CandleCon.y = Coord.y;
+        const candleBody = this.createCandleBody(CandleDetails,  color);
+        const needle = this.createNeedle(CandleDetails, h, color);
         CandleCon.addChild(needle, candleBody);
 
         return CandleCon
     }
 
-    createNeedle(NeedleDetails:NeedleDetails, color:CandleColor){
+    createNeedle(NeedleDetails:NeedleDetails, h:number, color:CandleColor){
         const needle = PIXI.Sprite.from(PIXI.Texture.WHITE);
         needle.tint = color;
         needle.x = 0;
-        needle.y = 0;
+        needle.y = h;
         needle.width = 1.5;
         needle.height = Math.abs(NeedleDetails.high - NeedleDetails.low);
         color == CandleColor.Green ? needle.anchor.set(.5, 0) : needle.anchor.set(.5, 1);
         return needle
     }
 
-    createCandleBody(CandleBodyDetails:CandleBodyDetails, h:number, color:CandleColor){
-        const height = CandleBodyDetails.close - CandleBodyDetails.open;
+    createCandleBody(CandleBodyDetails:CandleBodyDetails,  color:CandleColor){
+        const height = Math.abs(CandleBodyDetails.close - CandleBodyDetails.open);
         const candleBody = PIXI.Sprite.from(PIXI.Texture.WHITE);
         candleBody.tint = color;
         candleBody.x = 0;
-        candleBody.y =  h;
+        candleBody.y =  0;
         candleBody.width = 12;
-        candleBody.height = height
+        candleBody.height = height;
         color == CandleColor.Green ? candleBody.anchor.set(.5, 0) : candleBody.anchor.set(.5, 1);
+        
         return candleBody
     }
 }
